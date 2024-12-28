1196aa6fde989c21be9db6abb2759abbfba5fa90
luy06D <cusiluis04@gmail.com>
2024-12-28 00:56:50 -0500

    Avanze Sidebard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import {Button} from "@nextui-org/react";
 import './App.css'
-import {MyRoutes} from './routers/routes'
+import MyRoutes from './routers/routes'
+import styled, { ThemeProvider } from "styled-components";
+import {BrowserRouter} from 'react-router-dom'
+import Sidebar from "./components/Sidebar";
+import React, { useState } from "react";
+import { Light, Dark } from './styles/Themes';
+
+export const ThemeContext = React.createContext(null);
+
 
 function App() {
+
+  const [theme, setTheme] = useState("light");
+  const themeStye = theme === "light"? Light : Dark;
   
+  const changeTheme =()=>{
+    setTheme((theme) => (theme==="light"?"dark":"light"))
+  }
+
   return (  
      <>
-      <MyRoutes />
+     <ThemeContext.Provider value={{setTheme, theme}}>
+      <ThemeProvider theme={themeStye}>
+        <BrowserRouter>
+          <Container>
+            <input type='checkbox' onClick={changeTheme} />
+            <Sidebar/>
+            <MyRoutes />
+          </Container>
+        </BrowserRouter>
+      </ThemeProvider>
+     </ThemeContext.Provider>
+ 
      </>
   )
 }
+
+const Container = styled.div`
+background-color:${({theme}) =>theme.body}`;  
 
 export default App
--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import {Inicio} from '../pages/Inicio'
-import {Proyectos} from '../pages/Proyectos'
-import {Tareas} from '../pages/Tareas'
-import {Sidebar} from '../components/Sidebar'
+import Inicio from '../pages/Inicio'
+import Proyectos from '../pages/Proyectos'
+import Tareas from '../pages/Tareas'
 
 const MyRoutes = () => {
   return (
-    <BrowserRouter>
-      <Sidebar/>
+
         <Routes>
             <Route path='/' element={<Inicio />}/>
             <Route path='/proyectos' element={<Proyectos />}/>
             <Route path='/tareas' element={<Tareas />}/>
         </Routes>
-    </BrowserRouter>
   )
 }
 

df3e89833cf0dc7a291edbb083578b7e029611c0
luy06D <cusiluis04@gmail.com>
2024-12-29 23:03:43 -0500

    Avanze style Siderbar btn fecha

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 import logo from '../assets/react.svg'
 import {v} from '../styles/Variables'
+import { AiOutlineLeft } from "react-icons/ai";
 
 
 const Sidebar = ({sidebarOpen, setSidebarOpen }) => {
   return (
     <Container isOpen= {sidebarOpen}> 
+      <button className="SiderbarButton">
+        <AiOutlineLeft/>
+      </button>
       <div className='logoContent'>
         <div className='img_content'>
           <img src={logo} alt="" />
         </div>
-        <h2>Market</h2>
+        <h2>MARKETING</h2>
 
       </div>
     </Container>
@@ -22,6 +26,28 @@ const Container = styled.div`
   color:${(props) =>props.theme.text};
   background:${(props) =>props.theme.primary};
   position: sticky;
+  padding-top: 20px;
+
+  .SiderbarButton{
+    position: absolute;
+    top: ${v.xxlSpacing};
+    right: -18px;
+    width: 32px;
+    height: 32px ;
+    border-radius: 50%;
+    background: ${(props)=>props.theme.bgtgderecha};
+    box-shadow: 0 0 4px ${(props)=>props.theme.bg3}, 
+    0 0 7px ${(props)=>props.theme.bg};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transition: all 0.3s;
+    transform: ${({isOpen}) => (isOpen? `initial` : `rotate(180deg)`)} ;
+
+     
+  }
+
 
   .logoContent{
     display: flex;
@@ -33,7 +59,7 @@ const Container = styled.div`
       display: flex;
       cursor: pointer;
       transition: all 0.3s;
-      transform: ${({isOpen}) =>(isOpen? `scale(1.5)`: `scale(0.7)`)};
+      transform: ${({isOpen}) =>(isOpen? `scale(0.7)`: `scale(0.5)`)};
 
       img{
         max-width: 100%;
